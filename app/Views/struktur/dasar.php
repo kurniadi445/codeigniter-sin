b2405dc1fbfc52b5dfac339a9957f049e871cc42
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-06-10 17:40:21 +0700

    Tambah view untuk halaman siswa

---
+++ app/Views/struktur/dasar.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <!-- meta -->
+    <meta charset="utf-8">
+    <meta content="IE=edge" http-equiv="x-ua-compatible">
+    <meta content="initial-scale=1, width=device-width" name="viewport">
+    <!-- meta -->
+    <title>Sistem Informasi Nilai</title>
+    <!-- css -->
+    <link
+        href="<?= base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>"
+        rel="stylesheet"
+    >
+    <link
+        href="<?= base_url('assets/bower_components/font-awesome/css/font-awesome.min.css'); ?>"
+        rel="stylesheet"
+    >
+    <link href="<?= base_url('assets/bower_components/Ionicons/css/ionicons.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/dist/css/AdminLTE.min.css'); ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/dist/css/skins/_all-skins.min.css'); ?>" rel="stylesheet">
+    <?= $this->renderSection('css'); ?>
+    <!-- css -->
+    <!-- js -->
+    <script src="<?= base_url('assets/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
+    <script src="<?= base_url('assets/bower_components/jquery-ui/jquery-ui.min.js'); ?>"></script>
+    <!-- js -->
+    <!-- font -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic"
+        rel="stylesheet"
+    >
+    <!-- font -->
+    <script>
+        const site_url = window.location.origin;
+    </script>
+</head>
+<body class="hold-transition sidebar-collapse sidebar-mini skin-purple-light">
+<div class="wrapper">
+    <!-- header -->
+    <?= $this->include('struktur/komponen/header'); ?>
+    <!-- header -->
+    <!-- aside -->
+    <?= $this->include('struktur/komponen/aside'); ?>
+    <!-- aside -->
+    <!-- pembungkus konten -->
+    <div class="content-wrapper">
+        <?= $this->renderSection('konten'); ?>
+    </div>
+    <!-- pembungkus konten -->
+</div>
+<!-- js -->
+<script src="<?= base_url('assets/bower_components/jquery/dist/jquery.min.js'); ?>"></script>
+<script src="<?= base_url('assets/bower_components/bootstrap/dist/js/bootstrap.min.js'); ?>"></script>
+<script src="<?= base_url('assets/dist/js/adminlte.min.js'); ?>"></script>
+<?= $this->renderSection('js'); ?>
+<!-- js -->
+</body>
---
+++ app/Views/struktur/komponen/aside.php
@@ -0,0 +1,39 @@
+<aside class="main-sidebar">
+    <!-- sidebar -->
+    <section class="sidebar">
+        <!-- sidebar panel pengguna -->
+        <div class="user-panel">
+            <div class="image pull-left">
+                <img alt="Foto" class="img-circle" src="<?= base_url('assets/dist/img/nophoto.png'); ?>">
+            </div>
+            <div class="info pull-left">
+                <p>Admin</p>
+                <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
+            </div>
+        </div>
+        <!-- sidebar panel pengguna -->
+        <!-- menu sidebar -->
+        <ul class="sidebar-menu" data-widget="tree">
+            <li class="header text-uppercase">Menu Utama</li>
+            <li class="treeview">
+                <a href="#">
+                    <i class="glyphicon glyphicon-user"></i>
+                    <span>Siswa</span>
+                    <span class="pull-right-container">
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </span>
+                </a>
+                <ul class="treeview-menu">
+                    <li>
+                        <a href="<?= site_url('siswa'); ?>">
+                            <i class="fa fa-circle-o text-red"></i>
+                            <span>Master Siswa</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+        <!-- menu sidebar -->
+    </section>
+    <!-- sidebar -->
+</aside>
