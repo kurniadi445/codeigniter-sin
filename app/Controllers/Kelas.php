4be24b28ce05e9f6ae88b003f831cfb29ae4d803
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-06-10 19:08:12 +0700

    Tambah controller untuk halaman kelas

---
+++ app/Controllers/Kelas.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controllers;
+
+use Config\Database;
+
+class Kelas extends BaseController
+{
+    public function indeks(): string
+    {
+        $db = Database::connect();
+
+        $kelas = $db->table('kelas');
+        $kelas = $kelas->orderBy('nama_kelas');
+        $kelas = $kelas->get();
+        $kelas = $kelas->getResult();
+
+        return view('tampilan/kelas/indeks', [
+            'kelas' => $kelas
+        ]);
+    }
+}
