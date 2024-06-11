e723e7209b21e8510105c8ffe4d5850e225b687b
Ananda Rizky Kurniawan <qq.kurniadi445@gmail.com>
2024-06-11 17:58:16 +0700

    Update tahun ajaran pada halaman edit siswa

--- app/Views/tampilan/admin/siswa/edit.php
+++ app/Views/tampilan/admin/siswa/edit.php
@@ -210,7 +210,13 @@
                 <div class="row">
                     <div class="form-group col-sm-6">
                         <label for="masukan-tahun-ajaran">Tahun Ajaran</label>
-                        <input class="form-control" id="masukan-tahun-ajaran" name="ajaran" type="text">
+                        <input
+                            class="form-control"
+                            id="masukan-tahun-ajaran"
+                            name="ajaran"
+                            type="number"
+                            value="<?= $siswa->tahun_ajaran; ?>"
+                        >
                     </div>
                     <div class="form-group col-sm-6"></div>
                 </div>
